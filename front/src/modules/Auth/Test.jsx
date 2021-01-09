d41f9bde15ac1afec370e44b71211694e9561318
Tadzio123 <tadzio806@gmail.com>
2021-01-09 23:29:25 +0100

    BIG changes

--- front/src/modules/Auth/Test.jsx
+++ front/src/modules/Auth/Test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import GenericElement from '../../components/molecules/GenericElement';
+import GenericElement from 'components/molecules/GenericElement';
 
 const TestPage = () => (
   <GenericElement headline="Tadeusz Olszewski" subline="User" />
