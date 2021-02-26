7ca69cde6a009a6a300fee8c67d75c3172bae858
Piotr Żak <piotrzak77@gmail.com>
2021-02-26 07:59:13 +0100

    default route

--- front/src/modules/Root.jsx
+++ front/src/modules/Root.jsx
@@ -80,6 +80,7 @@ const Root = () => {
                 <Switch>
 
                  {/* to test  */}
+                 <Route path="/" component={LoginPage} />
                   <Route path={routes.login} component={LoginPage} />
                   <Route path={routes.forgotPassword} component={ForgotPasswordPage} />
                   <Route path={routes.resetPassword} component={ResetPasswordPage} />
