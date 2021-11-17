f8b008e0af9c2eca27ce2244ba875cce92e6de21
PiotrZak <piotrzak77@gmail.com>
2021-11-17 19:28:27 +0100

    cleanup

--- Planfi.Front/src/modules/Auth/Google/LoginHooks.jsx
+++ Planfi.Front/src/modules/Auth/Google/LoginHooks.jsx
@@ -9,18 +9,6 @@ const clientId =
 
 const clientSecret = "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";
 
-// const refreshTokenSetup = (res) => {
-//   let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
-
-//   const refreshToken = async () => {
-//     const newAuthRes = await res.reloadAuthResponse();
-//     refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
-//     localStorage.setItem('authToken', newAuthRes.id_token);
-//     setTimeout(refreshToken, refreshTiming);
-//   };
-//   setTimeout(refreshToken, refreshTiming);
-// };
-
 const timeToRedirectLogin = 1000;
 
 const LoginHooks = () => {
@@ -49,7 +37,6 @@ const LoginHooks = () => {
         console.log(error)
       });
 
-    //refreshTokenSetup(res);
   };
 
   const redirectToPage = (data) => {
@@ -62,15 +49,6 @@ const LoginHooks = () => {
     console.log('Login failed: res:', res);
   };
 
-  // const { signIn } = useGoogleLogin({
-  //   onSuccess,
-  //   onFailure,
-  //   clientId,
-  //   isSignedIn: true,
-  //   accessType: 'offline',
-  //   prompt: 'consent'
-  // });
-
   return (
     <div className="gm-container">
       <GoogleLogin
@@ -79,7 +57,6 @@ const LoginHooks = () => {
         onFailure={onFailure}
         cookiePolicy={'single_host_origin'}
       />
-      {/* <img onClick={signIn} src="/icons/library/gmail.svg" alt="google login" className="gm-icon"></img> */}
     </div>
   );
 }
