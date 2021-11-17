9402382acd2a6058d76b8fffd45881c6c4590454
PiotrZak <piotrzak77@gmail.com>
2021-11-17 20:59:16 +0100

    .Net Ts Generator

---
+++ Planfi.Front/src/Types/PlanfiApi/Models/ViewModels/ExerciseViewModel.tsx
@@ -0,0 +1,15 @@
+
+// NOTE: Type generated automatically. Please DON'T edit this file manually!
+import { Byte[] } from './Byte[]';
+import { Serie } from './Serie';
+
+export type ExerciseViewModel = {
+	exerciseId: string
+	name: string
+	description: string
+	files: Byte[][]
+	categoryId: string
+	categoryName: string
+	planId: string
+	series: Serie[]
+}
---
+++ Planfi.Front/src/Types/PlanfiApi/Models/ViewModels/UserViewModel.tsx
@@ -0,0 +1,16 @@
+
+// NOTE: Type generated automatically. Please DON'T edit this file manually!
+import { Role } from './Role';
+
+export type UserViewModel = {
+	userId: string
+	avatar: number[]
+	token: string
+	firstName: string
+	lastName: string
+	roleName: string
+	role: Role
+	email: string
+	phoneNumber: string
+	organizationId: string
+}
