3778db3e505a871677e8d9774da53f426497f90a
PiotrZak <piotrzak77@gmail.com>
2021-11-25 16:54:01 +0100

    Update Exercise.js

--- Planfi.Front/cypress/integration/Planfi.Front.Tests/Exercise.js
+++ Planfi.Front/cypress/integration/Planfi.Front.Tests/Exercise.js
@@ -1,41 +1,41 @@
-//const url = 'https://plan-fi.pl/'
-const url = 'localhost:3000/login'
-const longTitle = "SpB8rhVtJlnNpXEdTy41STMPA"
-const shortTitle = "Diamond Pomps"
+// //const url = 'https://plan-fi.pl/'
+// const url = 'localhost:3000/login'
+// const longTitle = "SpB8rhVtJlnNpXEdTy41STMPA"
+// const shortTitle = "Diamond Pomps"
 
 
-describe('Exercise Issues', () => {
-    before(() => {
-      cy.clearLocalStorageSnapshot()
-    })
+// describe('Exercise Issues', () => {
+//     before(() => {
+//       cy.clearLocalStorageSnapshot()
+//     })
   
-    beforeEach(() => {
-      cy.viewport(1280, 720)
-      cy.restoreLocalStorage()
-    })
+//     beforeEach(() => {
+//       cy.viewport(1280, 720)
+//       cy.restoreLocalStorage()
+//     })
   
-    afterEach(() => {
-      cy.saveLocalStorage()
-    })
+//     afterEach(() => {
+//       cy.saveLocalStorage()
+//     })
   
-    it('Login', () => {
-      cy.visit(url)
-      cy.get(':nth-child(1) > .sc-gsTCUz > .sc-gKsewC').type(
-        'jmeachem0@eventbrite.com'
-      )
-      cy.get(':nth-child(2) > .sc-gsTCUz > .sc-gKsewC').type('Jacklyn')
-      cy.get('.sc-dQppl').click()
-      cy.saveLocalStorage()
-      cy.wait(4000)
-    })
+//     it('Login', () => {
+//       cy.visit(url)
+//       cy.get(':nth-child(1) > .sc-gsTCUz > .sc-gKsewC').type(
+//         'jmeachem0@eventbrite.com'
+//       )
+//       cy.get(':nth-child(2) > .sc-gsTCUz > .sc-gKsewC').type('Jacklyn')
+//       cy.get('.sc-dQppl').click()
+//       cy.saveLocalStorage()
+//       cy.wait(4000)
+//     })
   
-    it('Add Exercise', () => {
-      cy.get('[href="/categories"]').click()
-      cy.get('.sc-jeGSBP').click()
-      cy.url().should('eq', 'https://plan-fi.pl/add-exercise')
-      cy.get('.sc-gKsewC').type(longTitle)
+//     it('Add Exercise', () => {
+//       cy.get('[href="/categories"]').click()
+//       cy.get('.sc-jeGSBP').click()
+//       cy.url().should('eq', 'https://plan-fi.pl/add-exercise')
+//       cy.get('.sc-gKsewC').type(longTitle)
 
-      cy.get('.sc-dQppl').click()
-    })
+//       cy.get('.sc-dQppl').click()
+//     })
   
-})
+// })
