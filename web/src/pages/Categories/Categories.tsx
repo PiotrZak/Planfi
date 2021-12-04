de9ef3a9eb4023f4239305cf3085cac4131f727b
Tomasz <t.prochowicz@selleo.com>
2021-12-04 22:21:52 +0100

    feat: create new project front

---
+++ web/src/pages/Categories/Categories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Categories: React.FC = () => {
+  return (
+    <div>
+      <div>Categories</div>
+    </div>
+  )
+}
+
+export default Categories
---
+++ web/src/pages/MyProfile/MyProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyProfile: React.FC = () => {
+  return (
+    <div>
+      <div>MyProfile</div>
+    </div>
+  )
+}
+
+export default MyProfile
